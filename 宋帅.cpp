5cc43e4aa51beb82d6e7f3949b73a7ee301fd5f0
DaSong <2042159974@qq.com>
2025-02-23 21:46:21 +0800

    test1

---
+++ 宋帅.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include<vector>
+#include<unordered_set>
+#include<unordered_map>
+using namespace std;
+class Solution {
+      public:
+
+    bool uniqueOccurrences(vector<int>& arr) {
+        unordered_map<int, int> occur;
+        for (const auto& x : arr) {
+            occur[x]++;
+        }
+        unordered_set<int> times;
+        for (const auto& x : occur) {
+            times.insert(x.second);
+        }
+        return times.size() == occur.size();
+    }
+};
+
