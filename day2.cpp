11fe0e7ae8d51001529aa6af4f86655315ded777
DaSong <2042159974@qq.com>
2025-03-15 21:53:32 +0800

    day 2

---
+++ day2.cpp
@@ -0,0 +1,14 @@
+﻿
+//遍历字符串 s，累加相邻字母 ASCII 值的绝对差，即为答案。
+
+class Solution {
+public:
+    int scoreOfString(string s) {
+        int ans = 0;
+        for (int i = 1; i < s.length(); i++) {
+            ans += abs(s[i] - s[i - 1]);
+        }
+        return ans;
+    }
+};
+
